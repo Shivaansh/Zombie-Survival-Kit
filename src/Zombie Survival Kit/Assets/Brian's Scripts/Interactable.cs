4c7238fa3cf31455d406e74c27f357b2eea7f5b7
jonatans <brianjonatan@DESKTOP-EH98R4U.localdomain>
2018-11-09 03:10:38 -0500

    Updated MIS

--- src/Zombie Survival Kit/Assets/Brian's Scripts/Interactable.cs
+++ src/Zombie Survival Kit/Assets/Brian's Scripts/Interactable.cs
@@ -13,7 +13,7 @@ public class Interactable : MonoBehaviour
     /* The minimum distance the player must be with the Interactable GameObject 
      * to interact with, and to keep the interaction with the GameObject.
      */
-    public float radius;
+    private float radius = 3f;
     /* Shows if the the Interactable GameObject is being focused on.
      */
     public bool isFocus = false;
@@ -62,7 +62,7 @@ public class Interactable : MonoBehaviour
     {
         isFocus = true;
         player = playerTransform;
-        hasInteracted = false;
+        hasInteracted = true;
     }
 
     /// <summary>
