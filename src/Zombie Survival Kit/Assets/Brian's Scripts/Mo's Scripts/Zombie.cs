5007ebc6d840467403d615a8a165eb9a51549bae
Mohammad <hussam17@mcmaster.ca>
2018-11-14 14:08:11 -0500

    Added enemy animations and dropping items

--- src/Zombie Survival Kit/Assets/Brian's Scripts/Mo's Scripts/Zombie.cs
+++ src/Zombie Survival Kit/Assets/Brian's Scripts/Mo's Scripts/Zombie.cs
@@ -5,10 +5,7 @@ using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
-    public float health;
-    public float damage;
     public bool campType;
-    public int level;
 
     public float detectRadius = 5f;
     public float returnToSpawnRadius = 25f;
@@ -21,6 +18,7 @@ public class Zombie : MonoBehaviour
 
     private CharacterCombat enemyCombat;
     private CharacterStats playerStats;
+    private ZombieStats zombieStats;
 
     private enum EnemyStates { Passive, Attack, Hurt, Dead };
     private EnemyStates enemyState = EnemyStates.Passive;
@@ -38,54 +36,62 @@ public class Zombie : MonoBehaviour
         player = PlayerStats.instance.player.transform;
         animator = GetComponent<Animator>();
         enemyCombat = GetComponent<CharacterCombat>();
+        zombieStats = GetComponent<ZombieStats>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        distanceToPlayer = Vector3.Distance(player.position, transform.position);
-        distanceToSpawn = Vector3.Distance(spawnLocation, transform.position);
-
-        switch (enemyState)
+        if (!zombieStats.isDeath())
         {
-            case EnemyStates.Passive:
+            distanceToPlayer = Vector3.Distance(player.position, transform.position);
+            distanceToSpawn = Vector3.Distance(spawnLocation, transform.position);
 
-                if (!agent.pathPending)
-                    if (agent.remainingDistance <= agent.stoppingDistance)
-                        if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
-                            RandomMovement();
+            switch (enemyState)
+            {
+                case EnemyStates.Passive:
 
-                if (distanceToPlayer <= detectRadius && distanceToSpawn < returnToSpawnRadius)
-                {
-                    enemyState = EnemyStates.Attack;
+                        if (!agent.pathPending)
+                            if (agent.remainingDistance <= agent.stoppingDistance)
+                                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                                    if (campType)
+                                        animator.SetBool("idle", true);
+                                    else
+                                        RandomMovement();
+
+                    if (distanceToPlayer <= detectRadius && distanceToSpawn < returnToSpawnRadius)
+                    {
+                        animator.SetBool("idle", false);
+                        enemyState = EnemyStates.Attack;
+                        MoveToTarget();
+
+                    }
+                    break;
+
+                case EnemyStates.Attack:
+
                     MoveToTarget();
 
-                }
-                break;
+                    if (distanceToPlayer <= (agent.stoppingDistance + 0.6f))
+                    {
+                        animator.SetBool("attack", true);
+                        playerStats = player.GetComponent<CharacterStats>();
 
-            case EnemyStates.Attack:
+                        if (playerStats != null)
+                            enemyCombat.Attack(playerStats);
+                    }
+                    else
+                        animator.SetBool("attack", false);
 
-                MoveToTarget();
-
-                if (distanceToPlayer <= (agent.stoppingDistance + 0.6f))
-                {
-                    animator.SetBool("attack", true);
-                    playerStats = player.GetComponent<CharacterStats>();
-
-                    if (playerStats != null)
-                        enemyCombat.Attack(playerStats);
-                }   
-                else
-                    animator.SetBool("attack", false);
-
-                if (distanceToSpawn >= returnToSpawnRadius)
-                {
-                    LookAtTarget(spawnLocation);
-                    agent.SetDestination(spawnLocation);
-                    enemyState = EnemyStates.Passive;
-                }
-                break;
-        }   
+                    if (distanceToSpawn >= returnToSpawnRadius)
+                    {
+                        LookAtTarget(spawnLocation);
+                        agent.SetDestination(spawnLocation);
+                        enemyState = EnemyStates.Passive;
+                    }
+                    break;
+            }
+        }
 	}
 
     void MoveToTarget()
@@ -107,29 +113,13 @@ public class Zombie : MonoBehaviour
 
     void RandomMovement()
     {
-        randomAngle = Random.Range(0f, Mathf.PI * 2f);
-        target = new Vector3(spawnLocation.x + 5f*Mathf.Sin(randomAngle), spawnLocation.y, spawnLocation.z + 5f*Mathf.Cos(randomAngle));
-        LookAtTarget(target);
-        agent.SetDestination(target);
-    }
-
-    public void AttackEnd()
-    {
-        //HealthManager.instance.TakeDamage(4);
-    }
-    public float ReturnHealth()
-    {
-        return health * level;
-    }
-
-    public float ReturnDamage()
-    {
-        return damage * level;
-    }
-
-    public int ReturnLevel()
-    {
-        return level;
+        if (!zombieStats.isDeath())
+        {
+            randomAngle = Random.Range(0f, Mathf.PI * 2f);
+            target = new Vector3(spawnLocation.x + 5f * Mathf.Sin(randomAngle), spawnLocation.y, spawnLocation.z + 5f * Mathf.Cos(randomAngle));
+            LookAtTarget(target);
+            agent.SetDestination(target);
+        }
     }
 
     private void OnDrawGizmos()
--- src/Zombie Survival Kit/Assets/Brian's Scripts/Mo's Scripts/ZombieStats.cs
+++ src/Zombie Survival Kit/Assets/Brian's Scripts/Mo's Scripts/ZombieStats.cs
@@ -4,10 +4,138 @@ using UnityEngine;
 
 public class ZombieStats : CharacterStats
 {
+    private Animator animator;
+    public float delay;
+    private bool isDead = false;
+    private bool isDropped = false;
+
+    //Brian's Code
+    private Item[] Drops;
+    public Transform zombie;
+
+    //Shiv's Code
+    float timeAnimStarted;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        Item headArmor = ScriptableObject.CreateInstance<Item>();
+        Item meleeWeapon = ScriptableObject.CreateInstance<Item>();
+        Item rangeWeapon = ScriptableObject.CreateInstance<Item>();
+        Item watermelon = ScriptableObject.CreateInstance<Item>();
+
+        headArmor = Resources.Load<Item>("Items/HeadArmor");
+        meleeWeapon = Resources.Load<Item>("Items/MeleeWeapon");
+        rangeWeapon = Resources.Load<Item>("Items/RangeWeapon");
+        watermelon = Resources.Load<Item>("Items/Watermelon");
+
+        curHealth = maxHealth;
+
+        Drops = new Item[4];
+        Drops[0] = headArmor;
+        Drops[1] = meleeWeapon;
+        Drops[2] = rangeWeapon;
+        Drops[3] = watermelon;
+    }
+
+    private void Update()
+    {
+        
+        if (Time.time >= timeAnimStarted + delay && isDead == true)
+        {
+            Vector3 deathLocation = zombie.transform.position;
+
+            Destroy(gameObject);
+
+            System.Random r = new System.Random();
+            int dropChoice = r.Next(0, 4);
+
+            DropItem(deathLocation, dropChoice);
+        }
+    }
+
     public override void Die()
     {
+        zombie.GetComponent<Enemy>().enabled = false;
+        isDead = true;
+        animator.SetBool("dead", true);
+        timeAnimStarted = Time.time;
+
+        
+
         base.Die();
-        //Add death animation, loot
-        Destroy(gameObject);
+        //Destroy(gameObject, delay);
+        
+
+        
+
+        //DropItem(deathLocation, dropChoice);
+
+    }
+
+    private void DropItem(Vector3 deathLocation, int dropChoice)
+    {
+        if (!isDropped)
+        {
+            isDropped = true;
+
+            switch (dropChoice)
+            {
+                case 0:
+                    Debug.Log("Dropped HeadArmor");
+                    GameObject HeadArmor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    HeadArmor.name = "HeadArmor";
+                    HeadArmor.transform.localScale = new Vector3(0.3948148f, 0.3948148f, 0.3948148f);
+                    HeadArmor.AddComponent<ItemStore>();
+                    HeadArmor.GetComponent<ItemStore>().item = Drops[dropChoice];
+                    HeadArmor.GetComponent<ItemStore>().radius = 3;
+                    HeadArmor.AddComponent<Rigidbody>();
+                    HeadArmor.GetComponent<Rigidbody>().useGravity = true;
+                    HeadArmor.transform.position = deathLocation;
+                    break;
+                case 1:
+                    Debug.Log("Dropped MeleeWeapon");
+                    GameObject MeleeWeapon = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    MeleeWeapon.name = "MeleeWeapon";
+                    MeleeWeapon.transform.localScale = new Vector3(0.3948148f, 0.3948148f, 0.3948148f);
+                    MeleeWeapon.AddComponent<ItemStore>();
+                    MeleeWeapon.GetComponent<ItemStore>().item = Drops[dropChoice];
+                    MeleeWeapon.GetComponent<ItemStore>().radius = 3;
+                    MeleeWeapon.AddComponent<Rigidbody>();
+                    MeleeWeapon.GetComponent<Rigidbody>().useGravity = true;
+                    MeleeWeapon.transform.position = deathLocation;
+                    break;
+                case 2:
+                    Debug.Log("Dropped RangeWeapon");
+                    GameObject RangeWeapon = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    RangeWeapon.name = "RangeWeapon";
+                    RangeWeapon.transform.localScale = new Vector3(0.3948148f, 0.3948148f, 0.3948148f);
+                    RangeWeapon.AddComponent<ItemStore>();
+                    RangeWeapon.GetComponent<ItemStore>().item = Drops[dropChoice];
+                    RangeWeapon.GetComponent<ItemStore>().radius = 3;
+                    RangeWeapon.AddComponent<Rigidbody>();
+                    RangeWeapon.GetComponent<Rigidbody>().useGravity = true;
+                    RangeWeapon.transform.position = deathLocation;
+                    break;
+                case 3:
+                    Debug.Log("Dropped Watermelon");
+                    GameObject Watermelon = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    Watermelon.name = "Watermelon";
+                    Watermelon.transform.localScale = new Vector3(0.3948148f, 0.3948148f, 0.3948148f);
+                    Watermelon.AddComponent<ItemStore>();
+                    Watermelon.GetComponent<ItemStore>().item = Drops[dropChoice];
+                    Watermelon.GetComponent<ItemStore>().radius = 3;
+                    Watermelon.AddComponent<Rigidbody>();
+                    Watermelon.GetComponent<Rigidbody>().useGravity = true;
+                    Watermelon.transform.position = deathLocation;
+                    break;
+            }
+        }
+    }
+
+    public bool isDeath()
+    {
+        return isDead;
     }
 }
