90b8496e93f125da7f89e38a6f22d6eb93ef68d9
jonatans <brianjonatan@DESKTOP-EH98R4U.localdomain>
2018-11-26 19:59:18 -0500

    Brian: Updated src files with Axe and Gun UI

---
+++ src/Zombie Survival Kit/Assets/Scripts/Tests/Test_CharacterCombat.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Test_CharacterCombat
+{
+    [UnityTest]
+    public IEnumerator Test_Attack()
+    {
+        GameObject GameManager = GameObject.Instantiate(Resources.Load<GameObject>("PrefabPlayer/GameManager"));
+        GameObject character = GameObject.Instantiate(Resources.Load<GameObject>("PrefabPlayer/PrimaryPlayer"));
+        GameObject character2 = GameObject.Instantiate(Resources.Load<GameObject>("PrefabPlayer/PrimaryPlayer"));
+        CharacterCombat attacker = character.GetComponent<CharacterCombat>();
+        CharacterStats attackerStats = character.GetComponent<CharacterStats>();
+        CharacterStats defenderStats = character2.GetComponent<CharacterStats>();
+        yield return null;
+
+        int oldHealth = defenderStats.curHealth;
+        attacker.Attack(defenderStats);
+        yield return new WaitForSeconds(2);
+        Assert.True(defenderStats.curHealth == defenderStats.maxHealth - attackerStats.dmg.GetValue());
+
+    }
+
+    [TearDown]
+    public void AfterEveryTest()
+    {
+        foreach (var gameobject in GameObject.FindGameObjectsWithTag("GameManager"))
+        {
+            Object.Destroy(gameobject);
+        }
+        foreach (var gameobject in GameObject.FindGameObjectsWithTag("GameController"))
+        {
+            Object.Destroy(gameobject);
+        }
+    }
+}
